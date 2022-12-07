69b0c74d76db27cfb04aa7068e79b5c0d87f8077
michael Ivanushkin <Michaeli106@nycstudents.net>
2022-12-07 13:13:50 -0500

    FSAfsa

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const DOMSelectors = {
     btn:document.getElementById("btn"),
     input:document.querySelector(`#input`),
-    box:document.getElementById("container")
+    box:document.getElementById("container"),
+    button:document.getElementById("btnd")
 }
 
-DOMSelectors.btn.addEventListener("click", function () {
+DOMSelectors.btn.addEventListener("click", function (s) {
     let task = DOMSelectors.input.value;
     DOMSelectors.box.insertAdjacentHTML("beforeend", `<p> ${task} <button type="button" id="btnd">-</button> </p>  `)
     DOMSelectors.input.value = "";
 
 });
+const card = btn.p
 
+btnd.addEventListener("click", function(card){
+    let deleteb = card.deleteb
+    deleteb.element.element.remove();
+
+}
+
+);
