d9f2025e7f50bef81a226f2d8e36dad1b7e0a9e1
michael Ivanushkin <Michaeli106@nycstudents.net>
2022-12-08 13:16:01 -0500

    eg

--- index.js
+++ index.js
@@ -2,21 +2,27 @@ const DOMSelectors = {
     btn:document.getElementById("btn"),
     input:document.querySelector(`#input`),
     box:document.getElementById("container"),
-    button:document.getElementById("btnd")
+    btnd:document.getElementById("btnd")
 }
 
-DOMSelectors.btn.addEventListener("click", function (s) {
+DOMSelectors.btn.addEventListener("click", function () {
     let task = DOMSelectors.input.value;
     DOMSelectors.box.insertAdjacentHTML("beforeend", `<p> ${task} <button type="button" id="btnd">-</button> </p>  `)
     DOMSelectors.input.value = "";
 
 });
-const card = btn.p
 
-btnd.addEventListener("click", function(card){
-    let deleteb = card.deleteb
-    deleteb.element.element.remove();
 
+// DOMSelectors.btnd.addEventListener("click", function(){
+// let card = document.p
+//     p.btnd.parentelement.remove();
+
+// });
+
+
+const remove = document.getElementById("btnd");
+remove.forEach((btnd) => {
+    btnd.addEventListener("click", function(){
+        btnd.parentelemet.remove();
 }
-
 );
