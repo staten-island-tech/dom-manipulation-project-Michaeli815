89c0663591369d9f9f42ebbfe60cca119fb1a38a
michael Ivanushkin <Michaeli106@nycstudents.net>
2022-12-09 13:14:16 -0500

    saavs

--- index.js
+++ index.js
@@ -2,27 +2,27 @@ const DOMSelectors = {
     btn:document.getElementById("btn"),
     input:document.querySelector(`#input`),
     box:document.getElementById("container"),
-    btnd:document.getElementById("btnd")
+    btnd:document.querySelectorAll("#btnd")
 }
 
 DOMSelectors.btn.addEventListener("click", function () {
     let task = DOMSelectors.input.value;
     DOMSelectors.box.insertAdjacentHTML("beforeend", `<p> ${task} <button type="button" id="btnd">-</button> </p>  `)
     DOMSelectors.input.value = "";
+});
+
+    let dbutton = document.DOMSelectors.btnd
+
+dbutton.addEventListener("click", function(del){
+    let dbutton = del.target;
+    dbutton.parentelement.remove();
 
 });
 
 
-// DOMSelectors.btnd.addEventListener("click", function(){
-// let card = document.p
-//     p.btnd.parentelement.remove();
-
-// });
-
-
-const remove = document.getElementById("btnd");
-remove.forEach((btnd) => {
-    btnd.addEventListener("click", function(){
-        btnd.parentelemet.remove();
-}
-);
+// const remove = document.getElementById("container");
+// remove.forEach((container) => {
+//     btnd.addEventListener("click", function(){
+//         btnd.parentelemet.remove();
+// }
+// );
