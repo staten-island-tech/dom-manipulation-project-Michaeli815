5fc712893fc9cb19857d90fcc9a36f1d966ac80b
michael Ivanushkin <Michaeli106@nycstudents.net>
2022-12-13 13:14:26 -0500

    rEH

--- index.js
+++ index.js
@@ -2,27 +2,37 @@ const DOMSelectors = {
     btn:document.getElementById("btn"),
     input:document.querySelector(`#input`),
     box:document.getElementById("container"),
-    btnd:document.querySelectorAll("#btnd")
 }
 
 DOMSelectors.btn.addEventListener("click", function () {
     let task = DOMSelectors.input.value;
-    DOMSelectors.box.insertAdjacentHTML("beforeend", `<p> ${task} <button type="button" id="btnd">-</button> </p>  `)
+    DOMSelectors.box.insertAdjacentHTML("beforeend", `<p> ${task}.
+     <button id="btnd">-</button> </p>  `)
+
+});
+
+// function carddef() {
+//     const card = DOMSelectors.querySelectorAll("p")
+// }
+
+
+DOMSelectors.btn.addEventListener("click", function () {
     DOMSelectors.input.value = "";
 });
 
-    let dbutton = document.DOMSelectors.btnd
+//     let dbutton = document.DOMSelectors.btnd
 
-dbutton.addEventListener("click", function(del){
-    let dbutton = del.target;
-    dbutton.parentelement.remove();
+// dbutton.addEventListener("click", function(del){
+//     let dbutton = del.target;
+//     dbutton.parentelement.remove();
 
+// });
+
+
+const remove = Array.from(document.querySelectorAll(`btnd`));
+const card = DOMSelectors.querySelectorAll("p")
+remove.forEach((buttond) => {
+    buttond.addEventListener("click", function(){
+        p.remove();
 });
-
-
-// const remove = document.getElementById("container");
-// remove.forEach((container) => {
-//     btnd.addEventListener("click", function(){
-//         btnd.parentelemet.remove();
-// }
-// );
+})
