885b1ae50fef511c9a530ed2e96bfd41303c7fea
michael Ivanushkin <Michaeli106@nycstudents.net>
2022-12-16 12:40:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,24 +16,10 @@ const tasks = {
 btnd: btnd  = "#btnd",
 card: card = "#card"
 }
-// const tasks = {
-//     btnd:querySelectorAll(`#btnd`),
-//     p:box.querySelectorAll(`#card`),
-
-
-// }
-// function carddef() {
-//     const card = DOMSelectors.querySelectorAll("p")
-// }
-
 
 DOMSelectors.btn.addEventListener("click", function () {
     DOMSelectors.input.value = "";
 });
-
-//     let dbutton = document.DOMSelectors.btnd
-
-
 
 function erase() {
 const remove = document.querySelectorAll(".btnd");
@@ -42,18 +28,3 @@ remove.forEach((buttond) => {
         event.target.parentElement.remove();
 });
 })};
-
-// const element = document.getElementById('btnd');
-// element.addEventListener("click", function () {
-// card.remove();
-// });
-
-// tasks.btnd.addEventListener("click", function(){
-//     card.element.element.remove();
-
-// });
-
-// btnd.addEventListener("click", function () {
-//     btnd.parentelement.remove();
-    
-// });
